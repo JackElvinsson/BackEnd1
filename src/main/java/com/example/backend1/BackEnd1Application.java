9b9acecb92162b9db2503d27e54132cf2bdacb40
JackElvinsson <jackelvinsson@gmail.com>
2023-04-24 13:50:32 +0200

    Initial commit

---
+++ src/main/java/com/example/backend1/BackEnd1Application.java
@@ -0,0 +1,13 @@
+package com.example.backend1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEnd1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEnd1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backend1/BackEnd1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEnd1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
