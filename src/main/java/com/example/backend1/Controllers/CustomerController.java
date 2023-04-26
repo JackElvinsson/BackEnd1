b83d3aeab20dabfe43a38cdc9cdd154badf5035b
Rasmus <rasmus.adolfsson99@live.se>
2023-04-26 12:17:45 +0200

    added GET-Endpoints for customer

--- src/main/java/com/example/backend1/Controllers/CustomerController.java
+++ src/main/java/com/example/backend1/Controllers/CustomerController.java
@@ -1,4 +1,31 @@
 package com.example.backend1.Controllers;
 
+import com.example.backend1.Models.Customer;
+import com.example.backend1.Repositories.CustomerRepo;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class CustomerController {
+
+    private final CustomerRepo customerRepo;
+
+
+    CustomerController(CustomerRepo customerRepo){
+        this.customerRepo = customerRepo;
+
+    }
+
+    @RequestMapping("customers")
+    public List<Customer> getAllCustomters(){
+        return customerRepo.findAll();
+    }
+
+    @RequestMapping("customers/{id}")
+    public Customer findCustomerById(@PathVariable Long id){
+        return customerRepo.findById(id).orElse(null);
+    }
 }
