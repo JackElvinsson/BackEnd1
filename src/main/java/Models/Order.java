49c082b0c18d9303e981c70cdedfad177e1f0317
Thomas Sjöberg <thomas.sjoberg91@gmail.com>
2023-04-26 10:57:38 +0200

    classses

---
+++ src/main/java/Models/Order.java
@@ -0,0 +1,27 @@
+package Models;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "purchase_order")
+public class Order {
+
+    @jakarta.persistence.Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    private String date;
+
+
+    @ManyToMany
+    @JoinTable
+    List<Item> itemList = new ArrayList<>();
+}
