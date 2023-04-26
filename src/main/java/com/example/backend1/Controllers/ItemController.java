1c319012a92d80d6aad145876d343d1929358a2d
Emma Vainio <emmavainio@live.se>
2023-04-26 20:27:43 +0200

    Get endpoints item

--- src/main/java/com/example/backend1/Controllers/ItemController.java
+++ src/main/java/com/example/backend1/Controllers/ItemController.java
@@ -1,4 +1,29 @@
 package com.example.backend1.Controllers;
 
+import com.example.backend1.Models.Item;
+import com.example.backend1.Repositories.ItemRepo;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class ItemController {
+
+    private final ItemRepo itemRepo;
+
+    public ItemController(ItemRepo itemRepo) {
+        this.itemRepo=itemRepo;
+    }
+
+    @RequestMapping("items")
+    public List<Item> getItems() {
+        return itemRepo.findAll();
+    }
+
+    @RequestMapping("items/{id}")
+    public Item getItemById(@PathVariable long id) {
+        return itemRepo.findById(id).orElseThrow(() -> new RuntimeException("No matching item found for id:" + id));
+    }
 }
---
+++ src/test/java/com/example/backend1/Controllers/ItemControllerTest.java
@@ -0,0 +1,62 @@
+package com.example.backend1.Controllers;
+
+import com.example.backend1.Models.Item;
+import com.example.backend1.Repositories.ItemRepo;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+
+import java.util.Arrays;
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ItemControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ItemRepo itemRepo;
+
+    @BeforeEach
+    public void init() {
+
+        Item i1 = new Item(1L,"Shoe", 130.5f);
+        Item i2 = new Item(2L,"Book", 200);
+        Item i3 = new Item(3L,"Game Boy", 999);
+
+        when(itemRepo.findAll()).thenReturn(Arrays.asList(i1,i2,i3));
+        when(itemRepo.findById(1L)).thenReturn(Optional.of(i1));
+        when(itemRepo.findById(2L)).thenReturn(Optional.of(i2));
+        when(itemRepo.findById(3L)).thenReturn(Optional.of(i3));
+
+    }
+
+    @Test
+    public void getAllItems() throws Exception {
+        mockMvc.perform(get("/items"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("[{\"id\": 1,\"name\": \"Shoe\",\"price\": 130.5}," +
+                        "{\"id\": 2,\"name\": \"Book\",\"price\": 200}," +
+                        "{\"id\": 3,\"name\": \"Game Boy\",\"price\": 999}]"));
+    }
+
+    @Test
+    public void getItemById() throws Exception {
+        mockMvc.perform(get("/items/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("{\"id\": 1,\"name\": \"Shoe\",\"price\": 130.5}"));
+    }
+
+}
